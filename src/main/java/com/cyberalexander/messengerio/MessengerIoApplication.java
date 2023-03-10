7299a1b6aaa6c877383fcf049c76ffec189e87d8
CyberAlexander <taurum13@gmail.com>
2023-03-10 09:29:05 +0300

    Link local project and github project

---
+++ src/main/java/com/cyberalexander/messengerio/MessengerIoApplication.java
@@ -0,0 +1,13 @@
+package com.cyberalexander.messengerio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyberalexander/messengerio/MessengerIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberalexander.messengerio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
