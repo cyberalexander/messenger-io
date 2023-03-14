a862fa6334dc0ca8c16b9e86bf395faf5607e6a2
CyberAlexander <taurum13@gmail.com>
2023-03-14 09:35:53 +0300

    Message Sending business logic implemented.

---
+++ src/main/java/com/cyberalexander/messengerio/TwilioMessageSender.java
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 CyberAlexander
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.cyberalexander.messengerio;
+
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created : 14/03/2023 09:28
+ * Project : messenger-io
+ * IDE : IntelliJ IDEA
+ *
+ * @author Aliaksandr_Leanovich
+ * @version 1.0
+ */
+@Slf4j
+@Service
+@AllArgsConstructor
+public class TwilioMessageSender implements MessageSender {
+
+    private final TwilioConfiguration twilioConfiguration;
+
+    @Override
+    public boolean sendMessage(MessageRequest request) {
+        Message message = Message.creator(
+                new PhoneNumber(request.getDestinationPhoneNumber()),
+                new PhoneNumber(twilioConfiguration.getFromNumber()),
+                request.getMessage()
+        ).create();
+        log.info(message.getErrorMessage());
+        return true;
+    }
+}
